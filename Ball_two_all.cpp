a8601d3e44fbbc4c68d23b82ab7f1e00f7788eb2
Pugovkina <tatiana_pugovkin@mail.ru>
2021-06-07 16:30:54 +0600

    Использование указателей для движения 2-х шаров одновременно

--- Ball_two_all.cpp
+++ Ball_two_all.cpp
@@ -2,8 +2,9 @@
 #include "TXLib.h"
 
 void Ball_two_way();
-void Ball_draw (int x, int y, int vx, int vy, COLORREF color, COLORREF fillcolor);
-void Ball_way  (int x, int y, int vx, int vy, int ax, int ay, int dt);
+void Ball_draw (int  x, int  y, int  vx, int  vy, COLORREF color, COLORREF fillcolor);
+void Ball_way  (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt);
+void Ball_Control (int* vx, int* vy);
 
 //-----------------------------------------------------------------
 
@@ -21,29 +22,26 @@ int main()
 void Ball_two_way()
     {
      int x1  = 100, y1  = 100,
-         vx1 = 5,   vy1 = 3;
+         vx1 = 5,   vy1 = 3,
+         ax1 = 0,   ay1 = 1;
 
      int x2  = 400, y2  = 400,
-         vx2 = 8,   vy2 = 5;
+         vx2 = 8,   vy2 = 5,
+         ax2 = 0,   ay2 = 1;
 
      int dt = 1;
 
      while (! txGetAsyncKeyState (VK_ESCAPE))
         {
-        Ball_draw (x1, y1, vx1, vy1, TX_LIGHTRED,  TXRED);
-        Ball_draw (x2, y2, vx2, vy2, TX_LIGHGREEN, TXGREEN);
+        Ball_draw (x1, y1, vx1, vy1, TX_LIGHTRED,   TX_RED);
+        Ball_draw (x2, y2, vx2, vy2, TX_LIGHTGREEN, TX_GREEN);
 
-        Ball_way (&x1, &y1, vx1, vy1, ax1, ay1, dt);
-        Ball_way (&x2, &y2, vx2, vy2, ax2, ay2, dt);
+        Ball_way (&x1, &y1, &vx1, &vy1, ax1, ay1, dt);
+        Ball_way (&x2, &y2, &vx2, &vy2, ax2, ay2, dt);
 
-        if (txGetAsyncKeyState (VK_RIGHT)) vx++;
-        if (txGetAsyncKeyState (VK_LEFT))  vx--;
-        if (txGetAsyncKeyState (VK_UP))    vy--;
-        if (txGetAsyncKeyState (VK_DOWN))  vy++;
+        Ball_Control (&vx1, &vy1);
 
-        if (txGetAsyncKeyState (VK_SPACE)) vx = vy = 0;
-
-        txSleep (1000);
+        txSleep (10);
         }
     }
 
@@ -55,41 +53,54 @@ void Ball_draw (int x, int y, int vx, int vy, COLORREF color, COLORREF fillcolor
     txSetFillColor (fillcolor);
 
     txCircle (x, y, 20);
+
     txLine (x, y, x + vx*5, y + vy*5);
     txCircle (x + vx*5, y+ vy*5, 3);
     }
 
 //-----------------------------------------------------------------
 
-void Ball_way (int x, int y, int vx, int vy, int ax, int ay, int dt)
+void Ball_way (int* x, int* y, int* vx, int* vy, int ax, int ay, int dt)
     {
-    vx += ax * dt;
-    vy += ay * dt;
+    *vx = *vx + ax * dt;
+    *vy = *vy + ay * dt;
 
-    x += vx * dt;
-    y += vy * dt;
+    *x = *x + *vx * dt;
+    *y = *y + *vy * dt;
 
-    if (x > 800)
+    if (*x > 800)
        {
-       vx = -vx;
-       x = 800;
+       *vx = - *vx;
+       *x  = 800;
        }
 
-    if (y > 800)
+    if (*y > 800)
        {
-       vy = -vy;
-       y = 800;
+       *vy = - *vy;
+       *y = 800;
        }
 
-    if (x < 0)
+    if (*x < 0)
        {
-       vx = -vx;
-       x = 0;
+       *vx = - *vx;
+       *x = 0;
        }
 
-    if (y < 0)
+    if (*y < 0)
        {
-       vy = -vy;
-       y = 0;
+       *vy = - *vy;
+       *y = 0;
        }
     }
+
+//-----------------------------------------------------------------
+
+void Ball_Control (int* vx, int* vy)
+   {
+   if (txGetAsyncKeyState (VK_RIGHT)) (*vx)++;
+   if (txGetAsyncKeyState (VK_LEFT))  (*vx)--;
+   if (txGetAsyncKeyState (VK_UP))    (*vy)--;
+   if (txGetAsyncKeyState (VK_DOWN))  (*vy)++;
+
+   if (txGetAsyncKeyState (VK_SPACE)) *vx = *vy = 0;
+   }
